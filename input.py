f59b461a4d15307d71254cb419ab567a0558bd4c
yhllyh <75822662+yhllyh@users.noreply.github.com>
2020-12-19 01:15:57 +0800

    修正換行問題以及前面的 bug
    bug: change = not change 才正確

--- input.py
+++ input.py
@@ -9,7 +9,7 @@ with open(file='data.txt', mode='r', encoding='utf-8') as f:
     for line in f:
         line = line.strip()
         if line == "ExpEnd" or line == "RevEnd":
-            change = not True
+            change = not change
             continue
         line = line.split()
         if change:
@@ -18,36 +18,27 @@ with open(file='data.txt', mode='r', encoding='utf-8') as f:
             exp.append(line)
 
 """處理新的資料"""
+"""
 print("Hello")
 print("To record expense please press 0")
 print("To record revenue please press 1")
 while True:
     add = input()
-
+"""
 """整理+儲存資料"""
 try:
     f = open(file="data.txt", mode='w', encoding='utf-8')
     for item in exp:
         line = " ".join(item)
-        f.write(line)
-    f.write("ExpEnd")
+        f.write(line + "\n")
+    f.write("ExpEnd\n")
     for item in rev:
         line = " ".join(item)
-        f.write(line)
-    f.write("RevEnd")
+        f.write(line + "\n")
+    f.write("RevEnd\n")
 finally:
     f.close()
 
 """TrashBin"""
 # print(rev)
 # print(exp)
-"""
-for item in exp:
-        line = " ".join(item)
-        f.write(line)
-    f.write("ExpEnd")
-    for item in rev:
-        line = " ".join(item)
-        f.write(line)
-    f.write("RevEnd")
-"""
