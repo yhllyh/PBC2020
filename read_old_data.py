2f02216687fec923a6a1de29c3cc864a019e5a06
yhllyh <75822662+yhllyh@users.noreply.github.com>
2020-12-22 20:41:40 +0800

    Create read_old_data.py
    分段

---
+++ read_old_data.py
@@ -0,0 +1,14 @@
+import gvar
+
+with open(file='data.txt', mode='r', encoding='utf-8') as f:
+    change = False
+    for line in f:
+        line = line.strip()
+        if line == "ExpEnd" or line == "RevEnd":
+            change = not change
+            continue
+        line = line.split()
+        if change:
+            gvar.rev.append(line)
+        else:
+            gvar.exp.append(line)
