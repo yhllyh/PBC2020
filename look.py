bd862a636361ed1203b72af4777d92f398b81657
yhllyh <75822662+yhllyh@users.noreply.github.com>
2020-12-22 14:35:38 +0800

    Update look.py
    add menubar

--- look.py
+++ look.py
@@ -2,4 +2,26 @@ import tkinter as tk
 
 root = tk.Tk()
 root.title("Test")
+
+
+def savefile():
+    pass
+
+
+def quitfile():
+    savefile()
+    frame.quit
+
+
+w = tk.Menubutton(root, text="Menu")
+w.pack()
+
+menubar = tk.Menu(root)
+menubar.add_command(label="主頁")
+menubar.add_command(label="明細")
+menubar.add_command(label="統計")
+menubar.add_command(label="退出", command=root.quit)
+root.config(menu=menubar)
+
+
 root.mainloop()
