1859ebfcdcd45e411eb686d3ad9e836a27393a5f
yhllyh <75822662+yhllyh@users.noreply.github.com>
2020-12-25 02:04:21 +0800

    Update main.py
    read_old_data 呼叫成功

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import gvar
 import read_old_data
-import add_new_data
-import update_data_file
+# import add_new_data
+# import update_data_file
+import timecalendar
+import tkinter as tk
+
+
+read_old_data.do()
+print(gvar.rev)
+
+root = tk.Tk()
+root.title("Test")
+
+menubar = tk.Menu(root)
+# menubar.add_command(label="主頁", command=homewin)
+# menubar.add_command(label="明細", command = )
+menubar.add_command(label="統計", command=statwin)
+# menubar.add_command(label="退出", command=root.quit)
+root.config(menu=menubar)
+
+root.mainloop()
